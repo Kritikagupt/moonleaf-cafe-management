73fe78cc7c1718e6cb8d26178d42a3a12bb4fe9b
Kritikagupt <kritikag802@gmail.com>
2026-02-16 17:53:28 +0530

    commit

--- Moonleafcafe/Moonleaf_cafe/urls.py
+++ Moonleafcafe/Moonleaf_cafe/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.Moonleaf_home, name="Moonleaf_cafe"),
-    path("location/", views.location, name="location"),
+    
     path("about/", views.about, name="about"),
     path("E-shop/", views.Eshop, name="E-shop"),
-    path("contact/", views.contact, name="contact"),
-    path("menu/", views.menu, name="menu"),
+    
 
 ]
--- Moonleafcafe/Moonleaf_cafe/views.py
+++ Moonleafcafe/Moonleaf_cafe/views.py
@@ -6,15 +6,9 @@ def Moonleaf_home(request):
 def about(request):
     return render(request, "Moonleaf_cafe/about.html")
 
-def location(request):
-    return render(request, "Moonleaf_cafe/location.html")
-
 def Eshop(request):
     return render(request, "Moonleaf_cafe/E-shop.html")
-def contact(request):
-    return render(request, "Moonleaf_cafe/contact.html")
-def menu(request):
-    return render(request, "Moonleaf_cafe/menu.html")
+
 
 
 
